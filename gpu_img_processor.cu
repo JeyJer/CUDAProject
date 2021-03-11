5f71b3abf9d51a7d38b023a1718ca606a85a0986
Gboho Thierry <thierry.gboho@etu.univ-orleans.fr>
2021-03-11 22:27:37 +0100

    efffet en cascade des filtres pour gpu

--- gpu_img_processor.cu
+++ gpu_img_processor.cu
@@ -19,9 +19,14 @@ int main(int argc, char **argv)
 
     cv::Mat m_in = cv::imread(img_in, cv::IMREAD_UNCHANGED);
 
-    unsigned char* rgb_in_aux = nullptr;
-    cudaMallocHost(&rgb_in_aux, 3 * m_in.rows * m_in.cols);
-    cv::Mat m_out(m_in.rows, m_in.cols, CV_8UC3, rgb_in_aux);
+    unsigned char* rgb_out = nullptr, *rgb_in = nullptr;
+    cudaMallocHost(&rgb_in, 3 * m_in.rows * m_in.cols);
+    cudaMallocHost(&rgb_out, 3 * m_in.rows * m_in.cols);
+
+    memcpy(rgb_in, m_in.data, 3 * m_in.rows * m_in.cols);
+
+    cv::Mat m_in_aux(m_in.rows, m_in.cols, CV_8UC3, rgb_in);
+    cv::Mat m_out(m_in.rows, m_in.cols, CV_8UC3, rgb_out);
 
     int (*fnc_exec) (cv::Mat&, cv::Mat&, GpuUtilExecutionInfo& );
 
@@ -31,12 +36,18 @@ int main(int argc, char **argv)
         else
             fnc_exec = GpuImgTransform::executeSharedMemMode;
     }else {
+        if( menuSelection.nb_stream > m_in.rows ) menuSelection.nb_stream = m_in.rows;
+
+        while(m_in.rows % menuSelection.nb_stream != 0) menuSelection.nb_stream--;
+
         if (!menuSelection.use_shared)
             fnc_exec = GpuImgTransformStream::execute;
         else
             fnc_exec = GpuImgTransformStream::executeSharedMemMode;
     }
     for( int i = 0; i < menuSelection.enabled_filters.size(); i++){
+        if( i > 0 ) swapPointers(&m_in_aux.data, &m_out.data);
+
         EffectStyle filter = menuSelection.enabled_filters.at(i);
         set_convolution_properties(info.conv_properties, filter);
         int conv_mat_length = info.conv_properties.size * info.conv_properties.size;
@@ -51,13 +62,14 @@ int main(int argc, char **argv)
 
         copyReverse(conv_mat, filter, conv_mat_length);
 
-        (*fnc_exec)(m_in, m_out, info );
+        (*fnc_exec)(m_in_aux, m_out, info );
 
     }
 
     cv::imwrite(img_out, m_out);
 
-    cudaFreeHost(rgb_in_aux);
+    cudaFreeHost(rgb_in);
+    cudaFreeHost(rgb_out);
 
     return 0;
 }
